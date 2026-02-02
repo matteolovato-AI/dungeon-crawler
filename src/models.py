d213758d5107cb13d720904dd85e83c5d20bad35
Anas El Badii <anas.elbadii@allievi.itsdigitalcademy.com>
2026-02-02 15:24:35 +0100

    creato entity e player

--- src/models.py
+++ src/models.py
@@ -1,1 +1,23 @@
 # classi
+class Entity:
+    def __init__(self, name: str, health: str,damage:int=0):
+        self.name = name
+        self.health = health
+        self.damage=damage
+    
+    def health_amount(self, damage: int) -> int:
+         self.health = int(self.health) - damage
+         
+         if(self.health<0):
+             self.health=0
+         return self.health
+
+class Player(Entity):
+    def __init__(self, name: str, health: str, position:tuple[int,int]):
+        super().__init__(name, health)
+        self.position = position
+
+        
+   
+        
+    
