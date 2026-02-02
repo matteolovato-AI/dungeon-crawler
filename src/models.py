9d594e0c5c4a6e82d25731d5530b6431128565a1
Matteo Lovato <matteo.lovato@allievi.itsdigitalacademy.com>
2026-02-02 15:29:31 +0100

    aggiunta classi room e map

--- src/models.py
+++ src/models.py
@@ -21,3 +21,40 @@ class Player(Entity):
    
         
     
+class Room:
+    def __init__(self, description) -> None:
+        self.description = description
+        self.items = []
+        self.enemies = []
+    def add_item(self, item):
+        
+        pass
+
+class Map:
+    def __init__(self, room: Room) -> None:
+        # creo la mappa con la prima stanza come inizio
+        self.map = {(0,0): room}
+        pass
+    def add_room(self, room: Room, room_position: tuple[int,int]) -> None:
+        self.map[room_position] = room
+    def check_room_neighbor_direction(self, position, direction) -> tuple[int,int] | None:
+
+        target_x, target_y = position[0], position[1]
+        if direction == "nord":
+            target_y += 1
+        elif direction == "sud":
+            target_y -= 1
+        elif direction == "est":
+            target_x += 1
+        elif direction == "ovest":
+            target_x -= 1
+        # ritorna la posizione della stanza adiacente
+        return (target_x, target_y) if (target_x >= 0 and target_y >= 0) else None
+
+    def check_room_neighbors(self, position) -> list[tuple[int,int]]:
+        neighbors = []
+        neighbors.append(self.check_room_neighbor_direction(position, 'nord'))
+        neighbors.append(self.check_room_neighbor_direction(position, 'sud'))
+        neighbors.append(self.check_room_neighbor_direction(position, 'est'))
+        neighbors.append(self.check_room_neighbor_direction(position, 'ovest'))
+        return neighbors
